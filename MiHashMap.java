80e7b268f44d02437d60c1db788a2e4f0c7899c2
adan291 <adanx05@gmail.com>
2016-03-07 13:38:16 +0100

    Creado e inicializado variables en los constructores

---
+++ MiHashMap.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+/**
+ * Write a description of class MiHashMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashMap
+{
+    // instance variables - replace the example below with your own
+    private String key[];
+    private int valores[];
+    /**
+     * Constructor for objects of class MiHashMap
+     */
+    public MiHashMap()
+    {
+        valores = new int[0];
+        key = new String [0];
+    }
+
+}
